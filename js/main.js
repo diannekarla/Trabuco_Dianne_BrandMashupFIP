ee52c6aaa6dedce36b1dcf5c2a79987940be6447
Dianne <diannekarlatrabuco@gmail.com>
2022-04-09 10:14:07 -0400

    FIP - Brand Mashup

--- js/main.js
+++ js/main.js
@@ -30,4 +30,86 @@ function displayFAQ() {
     const answers = document.getElementByClass('abswers')
     alert("asdasdasd");
     answers.classList.toggle("show")
-  }
+}
+
+function displayLabel(element){
+    target = element.id;
+    document.getElementById(target+"-label").classList.add("opacity-1");
+}
+
+function changeImage(element){
+    target = element.dataset.target;
+    document.getElementById("cap").src="assets/sliced-images/"+target+"/section-01.png";
+    document.getElementById("bottle").src="assets/sliced-images/"+target+"/section-02.png";
+    document.getElementById("beer").src="assets/sliced-images/"+target+"/section-03.png";
+}
+
+
+function showDesc(e){
+    var elems = document.querySelectorAll(".carousel-content-item"); //remove for all
+    [].forEach.call(elems, function(el) {
+        el.classList.remove("active");
+    });
+
+    document.getElementById(e).classList.add("active"); //add on clicked
+}
+
+
+function displayNavMobile() {
+    var x = document.getElementById("main-nav");
+    if (x.style.opacity === "1") {
+      x.style.opacity = "0";
+    } else {
+      x.style.opacity = "1";
+    }
+}
+
+function scrollNextFold(){
+    window.scrollTo({
+        top: 1000,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.addEventListener('load', function(){
+
+    /* About Us Slider*/
+    new Glider(document.querySelector('.glider'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.5,
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        },
+        dots: '.dots'
+    });
+
+
+    /* About Us Accordion*/
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(faqItem =>{
+        let btn = faqItem.querySelector('.question button');
+        let icon = faqItem.querySelector('.question button i');
+        var answer = faqItem.lastElementChild;
+        var answers = document.querySelectorAll('.faq-item .answer');
+        
+        btn.addEventListener('click', ()=>{
+            answers.forEach(ans =>{
+                let ansIcon = ans.parentElement.querySelector('button i');
+                if(answer !== ans){
+                    ans.classList.add('hidden');
+                    ansIcon.className = 'arrow down';
+                }
+            });
+            
+            answer.classList.toggle('hidden');
+            icon.className === 'arrow down' ? icon.className = 'arrow up' 
+            : icon.className ='arrow down';
+        });
+    });
+    
+});
+
+
